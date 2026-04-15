5b165271dea7f8edf66bb8fca9a8c25e5e6dd497
gerrald1410 <gerraldticoalu@gmail.com>
2026-04-15 12:18:45 +0700

    Update file PHP produk

--- update.php
+++ update.php
@@ -1,1 +1,13 @@
 <!-- Gerrald Ticoalu - 065124133 -->
+<?php
+include 'koneksi.php';
+
+$cek = mysqli_query($conn, "SHOW TABLES LIKE 'ilkom'");
+
+if(mysqli_num_rows($cek) > 0){
+    mysqli_query($conn, "ALTER TABLE ilkom RENAME TO ilmu_komputer");
+    echo "Berhasil rename!";
+} else {
+    echo "Tabel tidak ditemukan!";
+}
+?>
